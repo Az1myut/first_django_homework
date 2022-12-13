eaa85c865bb073b9691366424edd1fbaded51050
Пользователь <honeycomb0404@gmail.com>
2022-12-13 19:17:26 +0500

    First commit first django hw

---
+++ first_homework_1/first_homework_manager_1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FirstHomeworkManager1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'first_homework_manager_1'
---
+++ first_homework_2/first_homework_manager_2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FirstHomeworkManager2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'first_homework_manager_2'
